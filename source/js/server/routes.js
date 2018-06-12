50dd4c84f6b5ee07a166ef8e57e39d7994c91029
Rich Comber <rich@elementalwebdesign.co.uk>
2018-06-12 19:57:55 +0100

    Fixing paths and base app files

--- source/js/server/routes.js
+++ source/js/server/routes.js
@@ -1,6 +1,2 @@
-import { handleCookies } from './middleware/cookies';
-
 export default (app) => {
-  app.route('/dashboard').get(handleCookies, showDashboard);
-  app.route('/dashboard/*').get(handleCookies, showDashboard);
 };
