4dc88ecab9887dbc645ad52f1093c63357283b09
Rich Comber <rich@elementalwebdesign.co.uk>
2018-06-13 22:36:46 +0100

    Adding redux example

---
+++ source/js/apps/redux/routes/app/app.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Helmet } from 'react-helmet';
+import { Alerts } from '@salocreative/react-redux-alerts';
+import { translate } from 'react-i18next';
+
+// COMPONENTS
+import Menu from '../../components/menu';
+
+// HELPERS
+import renderRoutes from '../../../../components/app/renderRoutes';
+import routesConfig from '../index';
+import Appconfig from '../../config';
+
+class App extends React.Component {
+  componentDidUpdate(prevProps) {
+    const { location } = this.props;
+    if (location !== prevProps.location) {
+      window.scrollTo(0, 0);
+    }
+  }
+
+  render() {
+    const { match, systemAlerts } = this.props;
+    return (
+      <div className='App'>
+        <Helmet titleTemplate='%s | Salo Creative' />
+        <Menu match={ match } />
+        <div className='Page'>
+          <Alerts alerts={ systemAlerts } />
+          { renderRoutes(this.props, routesConfig) }
+        </div>
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  match: PropTypes.object.isRequired,
+  systemAlerts: PropTypes.array.isRequired
+};
+
+export default translate([Appconfig.name])(App);
--- source/js/server/routes.js
+++ source/js/server/routes.js
@@ -1,2 +1,6 @@
+import showRedux from './controllers/redux';
+
 export default (app) => {
+  app.route('/redux').get(showRedux);
+  app.route('/redux/*').get(showRedux);
 };
