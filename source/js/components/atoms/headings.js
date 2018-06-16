ed8142a429bd69f1e1ea06a1993de764e066ae5b
Rich Comber <rich@elementalwebdesign.co.uk>
2018-06-16 21:28:09 +0100

    Cleaning out more uneeded styles

---
+++ source/js/components/atoms/headings.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const H1 = styled.h1`
+  font-size: 32px;
+  margin: 30px 0 10px;
+  font-weight: 700;
+`;
+
+export const H2 = styled.h2`
+  font-size: 28px;
+  margin: 30px 0 10px;
+  font-weight: 700;
+`;
+
+export const H3 = styled.h3`
+  font-size: 24px;
+  margin: 30px 0 10px;
+  font-weight: 700;
+`;
+
+export const H4 = styled.h4`
+  font-size: 18px;
+  margin: 10px 0;
+  font-weight: 700;
+`;
+
+export const H5 = styled.h5`
+  font-size: 16px;
+  margin: 10px 0;
+  font-weight: 700;
+`;
+
+export const H6 = styled.h6`
+  font-size: 14px;
+  margin: 10px 0;
+  font-weight: 700;
+`;
--- source/js/components/index.js
+++ source/js/components/index.js
@@ -3,6 +3,7 @@ import Container from './atoms/container';
 import Row from './atoms/row';
 import Column from './atoms/column';
 import Sidebar from './atoms/sidebar';
+import { H1, H2, H3, H4, H5, H6 } from './atoms/headings';
 
 // MOLECULES
 import Logo from './molecules/logo';
@@ -13,6 +14,7 @@ export {
   Row,
   Column,
   Sidebar,
+  H1, H2, H3, H4, H5, H6,
   // Molecules
   Logo
 };
