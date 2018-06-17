e331c28ff8674c84322021c2744116762fb59dec
Rich Comber <rich@elementalwebdesign.co.uk>
2018-06-17 08:38:02 +0100

    Adding Middleware to handle cookies

--- source/js/helpers/index.js
+++ source/js/helpers/index.js
@@ -1,11 +1,13 @@
 import history from '../config/browserHistory';
 import serverHistory from '../config/serverHistory';
 
+// CHECK IF BROWSER
 export const isBrowser = !!(
   (typeof window !== 'undefined' &&
   window.document && window.document.createElement)
 );
 
+// PERFORM SSR SAFE REDIRECT
 export function ssrSafeRedirect(url) {
   if (isBrowser) {
     return history.push(url);
@@ -13,4 +15,12 @@ export function ssrSafeRedirect(url) {
   return serverHistory.push(url);
 }
 
+// GET NODE ENV
 export const ENV = process.env.NODE_ENV;
+
+// TOKEN FULLY EXPIRED
+export function tokenFullyExpired(expiry, time = 172800) {
+  if (!expiry) return false;
+  const timeDiff = Math.round((Date.now() - expiry) / 1000);
+  return timeDiff >= time;
+}
