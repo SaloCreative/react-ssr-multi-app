b507389dd69cccef881ac34b47b2427d9fcb500a
Rich Comber <rich@elementalwebdesign.co.uk>
2018-06-17 12:37:43 +0100

    Removing old client build

--- source/js/apps/redux/client.js
+++ source/js/apps/redux/client.js
@@ -1,4 +1,4 @@
-import buildClient from '../../helpers/buildClient';
+import buildClient from '../../client/buildReduxClient';
 import store from './config/store';
 import AppConfig from './config';
 import Client from './routes/app/client';
