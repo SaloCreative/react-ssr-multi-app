d031548b2b57cddaaa60d1cfe13afeb56fe6626f
Rich Comber <rich@elementalwebdesign.co.uk>
2018-06-17 14:30:26 +0100

    Adding graphql queries

--- source/js/apps/apollo/pages/home/index.js
+++ source/js/apps/apollo/pages/home/index.js
@@ -2,12 +2,68 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import { translate } from 'react-i18next';
+import { Query } from 'react-apollo';
 
 // COMPONENTS
 import Page from '../../containers/hoc/page';
-import { H1 } from '../../../../components';
+import { H1, H2, Button } from '../../../../components';
+
+// QUERIES
+import GET_POST from '../../queries/demo/getPost';
+import GET_USERS from '../../queries/demo/getUsers';
 
 class Home extends React.Component {
+  renderContent() {
+    return (
+      <Query
+        query={ GET_POST }
+        variables={ { id: 1 } }
+      >
+        { ({ loading, error, data }) => {
+              if (loading) return <p>Fetching</p>;
+              if (error) return `${ error }`;
+              return (
+                <React.Fragment>
+                  <H2>{ data.post.title }</H2>
+                  <p>{ data.post.body }</p>
+                </React.Fragment>
+              );
+            } }
+      </Query>
+    );
+  }
+
+  renderUsers() {
+    return (
+      <table width='100%'>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <Query
+            query={ GET_USERS }
+          >
+            { ({ loading, error, data }) => {
+            if (loading) return <tr><td colSpan={ 3 }>Fetching</td></tr>;
+            if (error) return `${ error }`;
+            return data.users.map((user) => (
+              <tr key={ user.id }>
+                <td>{ user.id }</td>
+                <td>{ user.name }</td>
+                <td>{ user.email }</td>
+              </tr>
+              ));
+            } }
+          </Query>
+        </tbody>
+      </table>
+    );
+  }
+
   render() {
     const { t } = this.props;
     return (
@@ -17,6 +73,9 @@ class Home extends React.Component {
         </Helmet>
         <div>
           <H1>{ t('HOME') }</H1>
+          <Button onClick={ () => console.log('Add alert') } fullWidth >Add an alert</Button>
+          { this.renderContent() }
+          { this.renderUsers() }
         </div>
       </React.Fragment>
     );
