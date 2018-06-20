7bf102776467634a047fa8a390b41ccc3c99efd3
Rich Comber <rich@elementalwebdesign.co.uk>
2018-06-20 14:40:23 +0100

    Adding dummy authenticated route

--- source/js/apps/apollo/config/pages.js
+++ source/js/apps/apollo/config/pages.js
@@ -3,13 +3,22 @@ import AppConfig from '../config';
 const base = `/${ AppConfig.name }/:language`;
 
 export const HOME = {
-  path: `${ base }`
+  path: `${ base }`,
+  title: 'HOME'
 };
 
 export const WHOOPS = {
-  path: `${ base }/whoops`
+  path: `${ base }/whoops`,
+  title: 'WHOOPS'
+};
+
+export const AUTHENTICATED_ROUTE = {
+  path: `${ base }/authenticated-route`,
+  title: 'AUTHENTICATED_ROUTE',
+  permissions: [1, 2, 3]
 };
 
 export const FOUROHFOUR = {
-  path: `${ base }/*`
+  path: `${ base }/*`,
+  title: '404'
 };
