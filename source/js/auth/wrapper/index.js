afa8e153d5f1d2f98822a34885c483c9e91f5a35
Rich Comber <rich@elementalwebdesign.co.uk>
2018-06-20 08:42:25 +0100

    Adding permissions evaluation to auth wrapper

--- source/js/auth/wrapper/index.js
+++ source/js/auth/wrapper/index.js
@@ -6,12 +6,16 @@ import { Consumer as AuthConsumer } from '../context';
 
 class AuthWrapper extends React.Component {
   render() {
-    const { children, authenticated } = this.props;
+    const { children, authenticated, permissions } = this.props;
     return (
       <AuthConsumer>
         { ({ loggedIn, hasPermissions }) => {
-          const isLoggedIn = loggedIn();
-          if ((isLoggedIn && authenticated) || (!isLoggedIn && !authenticated)) {
+          const isLoggedIn = loggedIn(); // Is the user logged in
+          let allowedAccess = (isLoggedIn && authenticated) || (!isLoggedIn && !authenticated); // Can they access
+          if (allowedAccess && authenticated && permissions.length) {
+            allowedAccess = hasPermissions(permissions); // Do they have permission
+          }
+          if (allowedAccess) {
             return (
               <React.Fragment>
                 { children }
